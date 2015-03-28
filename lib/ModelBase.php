dbe6ec39ad314fa5d8bb50c85d79a7ffc0179b1d
zhangfangxiong <472974245@qq.com>
2015-03-28 20:58:10 +0800

    修改

---
+++ lib/ModelBase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangfangxiong
+ * Date: 15/3/28
+ * Time: 下午8:16
+ */
+include_once("base.php");
+
+class ModelBase extends base
+{
+    function __construct()
+    {
+
+    }
+}
