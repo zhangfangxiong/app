7f7d32c6a028818b1f6d2a679f4434b3f7b401e5
zhangfangxiong <472974245@qq.com>
2015-03-28 18:31:55 +0800

    修改

--- lib/base.php
+++ lib/base.php
@@ -22,17 +22,30 @@ class base
         $this->$action();
     }
 
+    /**
+     * 赋值
+     * @param $sVar
+     * @param $mData
+     */
     protected function assign($sVar,$mData)
     {
         $this->tplVars[$sVar] = $mData;
     }
 
+    /**
+     * 加载模版
+     * @param $sFileName
+     */
     protected function display($sFileName)
     {
         $sTemplate = $GLOBALS['VIEWPATH'].$sFileName;
         echo $this->_run($sTemplate,$this->tplVars);
     }
 
+    /**
+     * 获取数据库连接
+     * @return DB|null
+     */
     protected function getDB()
     {
         if ($this->oDb == null) {
@@ -41,6 +54,10 @@ class base
         return $this->oDb;
     }
 
+    /**
+     * 获取memcache连接
+     * @return Lib_Memcache|null
+     */
     protected function getMem()
     {
         if ($this->oMem == null) {
@@ -49,11 +66,22 @@ class base
         return $this->oMem;
     }
 
+    /**
+     * 获取当前用户
+     * @return string
+     */
     protected function getCurrUser()
     {
         return "zfx";
     }
 
+    /**
+     * 渲染模版
+     * @param $template
+     * @param $vars
+     * @param bool $useEval
+     * @return string
+     */
     protected function _run ($template, $vars, $useEval = false)
     {
         if ($vars == null && count($this->tplVars) > 0) {
@@ -73,4 +101,55 @@ class base
         $content = ob_get_clean();
         return $content;
     }
+
+    /**
+     * 远程访问地址
+     * @param $url
+     * 访问url
+     */
+    protected function curl($sUrl)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_DNS_USE_GLOBAL_CACHE, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_URL, $sUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $content = curl_exec($ch);
+        $response = curl_getinfo($ch);
+        curl_close($ch);
+
+        return $content;
+    }
+
+    /**
+     * 处理XML格式数据，返回数组
+     * @param $xXml
+     * @return array
+     */
+    protected function dealxml($xXml) {
+        //转换为simplexml对象
+        $xmlResult = simplexml_load_string($xXml, null, LIBXML_NOCDATA);
+        //foreach循环遍历
+        $aNode = array();
+        foreach ($xmlResult->children() as $childItem) {
+            //输出xml节点名称和值
+            $item_arr = (array)$childItem;
+            $aNode[$childItem->getName()] = $item_arr[0];
+        }
+        return $aNode;
+    }
+
+    /*
+$content = simplexml_load_string('<content><![CDATA[Hello, world!]]></content>');
+echo (string) $content;
+
+$foo = simplexml_load_string('<foo><content><![CDATA[Hello, world!]]></content></foo>');
+echo (string) $foo->content;
+
+// 通过下面的方法自动过滤 CDATA 内部参数
+$content = simplexml_load_string('<content><![CDATA[Hello, world!]]></content>', null, LIBXML_NOCDATA);
+*/
 }
--- weixin/baseWeixin.php
+++ weixin/baseWeixin.php
@@ -14,10 +14,116 @@ include_once("../config/globalConfig.php");
 
 class baseWeixin extends base
 {
+
+    protected $tokenParamArr = array();
+    protected $AccessToken = "";//这个应该存到memcache里面
+
     const APPID = "wx9b2a25e390a27d33";
     const APPSECRET = "84796fa0c79fd86b5087263cbbb27268";
     const TOKEN = "aaaabbbbccccdddd";
     const ACCESSTOKENURL = "https://api.weixin.qq.com/cgi-bin/token";
     const GETWEIXINIPURL = "https://api.weixin.qq.com/cgi-bin/getcallbackip";
     const RESTYPE = "text";//回复的类型
+
+    public function __construct()
+    {
+        if ($this->checkSignature()) {
+            if (isset($_GET['echostr'])) {
+                //第一次配置的基础验证
+                echo $_GET['echostr'];
+                die;
+            }
+            if (isset($_GET['action']) && $_GET['action']) {
+                $action = $_GET['action'] . "Action";
+            } else {
+                $action = "receiveMsgAction";
+            }
+            $this->$action();
+        }
+    }
+
+    /**
+     * @return bool
+     * 验证配置信息是否正确
+     */
+    protected function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $token = self::TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 获取AccessToken
+     * @return bool|string
+     */
+    protected function getAccessToken()
+    {
+        if (!$this->AccessToken) {
+            $oMem = $this->getMem();
+            $sAccesstoken = $oMem->get("weixin_AccessToken");
+            if (!$sAccesstoken) {
+                $tokenParamArr = $this->tokenParamArr;
+                $tokenUrl = self::ACCESSTOKENURL;
+                $tokenParamArr['appid'] = self::APPID;
+                $tokenParamArr['secret'] = self::APPSECRET;
+                $tokenParamArr['grant_type'] = 'client_credential';
+                $tokenUrl .= "?" . http_build_query($tokenParamArr);
+                $data = $this->curl($tokenUrl);
+                $data = json_decode($data, true);
+                $oMem->set('weixin_AccessToken', $data['access_token'], 7200);
+                $sAccesstoken = $data['access_token'];
+            }
+            $this->AccessToken = $sAccesstoken;
+        }
+
+        return $this->AccessToken;
+    }
+
+    /**
+     * 配置信息修改日志
+     * @param $sData
+     */
+    protected function visitLog($sData)
+    {
+        $myfile = fopen("weixinVisitLog.txt", "a+") or die("Unable to open file!");
+        fwrite($myfile, $sData);
+        fclose($myfile);
+    }
+
+    /**
+     * 获取微信服务器IP地址
+     * @return mixed
+     */
+    protected function getWeixinIp()
+    {
+        $sUrl = self::GETWEIXINIPURL;
+        $sAccessToken = $this->getAccessToken();
+        $sUrl .= "?access_token=" . $sAccessToken;
+        $sData = $this->curl($sUrl);
+
+        return $sData;
+    }
+
+    /**
+     * 接收微信消息
+     * @return array
+     */
+    protected function receiveMsg()
+    {
+        //接收传送的数据
+        $fileContent = file_get_contents("php://input");
+        return $this->dealxml($fileContent);
+    }
 }
